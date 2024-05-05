fad6cadbbab538291857328c0d06e82ac20574f1
Ratnesh-Rajput <105653121+Ratnesh-Rajput@users.noreply.github.com>
2024-05-06 03:22:53 +0530

    Layout.jsx added

---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+    return (
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+    )
+}
+
+export default Layout
